3bfba08c82f055041064f8ccf6f2e18fd620529d
DenisCo1100100 <kodek_den2001@mail.ru>
2026-06-16 23:29:07 +0300

    fix: register get court by id query handler

---
+++ src/VolleyHub.Application/Courts/Queries/GetCourtById/GetCourtByIdQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using VolleyHub.Application.Common.Exceptions;
+using VolleyHub.Application.Common.Interfaces;
+using VolleyHub.Application.Courts.Dtos;
+using VolleyHub.Application.Courts.Mappings;
+using VolleyHub.Domain.Courts;
+
+namespace VolleyHub.Application.Courts.Queries.GetCourtById
+{
+    public sealed class GetCourtByIdQueryHandler
+        : IRequestHandler<GetCourtByIdQuery, CourtDto>
+    {
+        private readonly ICourtRepository _courtRepository;
+
+        public GetCourtByIdQueryHandler(ICourtRepository courtRepository)
+        {
+            _courtRepository = courtRepository;
+        }
+
+        public async Task<CourtDto> Handle(
+            GetCourtByIdQuery request,
+            CancellationToken cancellationToken)
+        {
+            var court = await _courtRepository.GetByIdAsync(
+                request.Id,
+                cancellationToken);
+
+            if (court is null)
+            {
+                throw new NotFoundException(nameof(Court), request.Id);
+            }
+
+            return court.ToDto();
+        }
+    }
+}
