c74426bbc042e051a3660fdc0ac625a2a02ff6dc
DenisCo1100100 <kodek_den2001@mail.ru>
2026-07-15 01:56:07 +0300

    feat: add player profile relation to game participants

---
+++ src/VolleyHub.Infrastructure/Persistence/Migrations/20260714225359_AddGameParticipantPlayerProfileRelation.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VolleyHub.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGameParticipantPlayerProfileRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddForeignKey(
+                name: "FK_game_participants_player_profiles_player_profile_id",
+                table: "game_participants",
+                column: "player_profile_id",
+                principalTable: "player_profiles",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_game_participants_player_profiles_player_profile_id",
+                table: "game_participants");
+        }
+    }
+}
